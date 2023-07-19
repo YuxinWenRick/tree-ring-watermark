99fad8e0257754fed667aaa90b2a2a61b5911e42
khlee0192 <khlee0192@gmail.com>
2023-07-19 16:44:28 +0900

    work on noise_optimizer.py

--- noise_optimizer.py
+++ noise_optimizer.py
@@ -15,16 +15,17 @@ class edcorrector(nn.Module):
         OUTPUT
         z : modified latent data
         """
-        
+    
         
         """ toy example : same as random noise
         self.fixed_x = x
         z = torch.randn_like(self.encoder(x))
         z = minimize(self.loss, z, method='bfgs')
         return z.x
-        """
-
-        # return self.encoder(x) # induces same result, will use this as framework
 
     def loss(self, z):
         return torch.norm((self.encoder(self.decoder(z)-self.fixed_x))**2 + self.lamb * (self.encoder(self.fixed_x) - z)**2)
+        """
+
+        # return self.encoder(x) # induces same result, will use this as framework
+        return z
