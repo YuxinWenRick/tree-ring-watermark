90ecef892b91e25be7a6a9a1b66d5433cf21f1d7
Christopher Antar <chrisa@nexuscml00.umiacs.umd.edu>
2023-10-12 15:07:43 -0400

    removed virtual environment activation in train and updated requirements.txt

--- train.sh
+++ train.sh
@@ -11,6 +11,5 @@
 #SBATCH --mem=32G
 
 
-conda activate treering
 module load cuda/10.2.89
 python run_tree_ring_watermark.py --run_name no_attack --w_channel 3 --w_pattern ring --start 0 --end 1000 --with_tracking --reference_model ViT-g-14  --reference_model_pretrain laion2b_s12b_b42k
